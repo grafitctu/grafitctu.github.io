d53d59c64f139e04d5b10c6ba88bcd38ed998411
richtrad <35641706+richtrad@users.noreply.github.com>
2022-04-03 05:20:48 +0200

    Update scrolll.js

--- lectures/scrolll.js
+++ lectures/scrolll.js
@@ -40,6 +40,22 @@ function setScroll() {
     meet2.innerHTML = '';
     meet2.prepend(before2);
     meet2.append(after2);
+    
+    <!--- --->
+
+    let meet3 = document.getElementById("meetings-title3");
+    let before3 = document.createElement("span");
+    before3.classList.add("navigation");
+    before3.innerHTML = '&larr; Modern Vis';
+    let after3 = document.createElement("span");
+    after3.classList.add("navigation");
+    after3.innerHTML = ' ualisation Technologies &rarr;';
+    meet3.innerHTML = '';
+    meet3.prepend(before3);
+    meet3.append(after3);
+    
+
+    
 
     let meetings = document.getElementById("meetings");
     let meeting = document.getElementsByClassName("meeting");
