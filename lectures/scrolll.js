98056bdf07f6338a45a61918cac5c4fbf4c83045
richtrad <35641706+richtrad@users.noreply.github.com>
2022-04-03 05:49:55 +0200

    Update scrolll.js

--- lectures/scrolll.js
+++ lectures/scrolll.js
@@ -49,11 +49,101 @@ function setScroll() {
     before3.innerHTML = '&larr; Modern Vis';
     let after3 = document.createElement("span");
     after3.classList.add("navigation");
-    after3.innerHTML = ' ualisation Technologies &rarr;';
+    after3.innerHTML = 'ualisation Technologies &rarr;';
     meet3.innerHTML = '';
     meet3.prepend(before3);
     meet3.append(after3);
     
+    <!--- --->
+
+    let meet4 = document.getElementById("meetings-title4");
+    let before4 = document.createElement("span");
+    before4.classList.add("navigation");
+    before4.innerHTML = '&larr; Modern Vis';
+    let after4 = document.createElement("span");
+    after4.classList.add("navigation");
+    after4.innerHTML = 'ualisation Technologies &rarr;';
+    meet4.innerHTML = '';
+    meet4.prepend(before4);
+    meet4.append(after4);
+    
+    <!--- --->
+
+    let meet3 = document.getElementById("meetings-title3");
+    let before3 = document.createElement("span");
+    before3.classList.add("navigation");
+    before3.innerHTML = '&larr; Virtual Gam';
+    let after3 = document.createElement("span");
+    after3.classList.add("navigation");
+    after3.innerHTML = 'ing Worlds &rarr;';
+    meet3.innerHTML = '';
+    meet3.prepend(before3);
+    meet3.append(after3);
+    
+    <!--- --->
+
+    let meet9 = document.getElementById("meetings-title9");
+    let before9 = document.createElement("span");
+    before9.classList.add("navigation");
+    before9.innerHTML = '&larr; Creative Coding and ';
+    let after9 = document.createElement("span");
+    after9.classList.add("navigation");
+    after9.innerHTML = 'Comitational Arts &rarr;';
+    meet9.innerHTML = '';
+    meet9.prepend(before9);
+    meet9.append(after9);
+    
+    <!--- --->
+
+    let meet5 = document.getElementById("meetings-title5");
+    let before5 = document.createElement("span");
+    before5.classList.add("navigation");
+    before5.innerHTML = '&larr; Architecture of';
+    let after5 = document.createElement("span");
+    after5.classList.add("navigation");
+    after5.innerHTML = 'Computer Games &rarr;';
+    meet5.innerHTML = '';
+    meet5.prepend(before5);
+    meet5.append(after5);
+    
+    <!--- --->
+
+    let meet6 = document.getElementById("meetings-title6");
+    let before6 = document.createElement("span");
+    before6.classList.add("navigation");
+    before6.innerHTML = '&larr; Pattern ';
+    let after6 = document.createElement("span");
+    after6.classList.add("navigation");
+    after6.innerHTML = 'Recognition &rarr;';
+    meet6.innerHTML = '';
+    meet6.prepend(before6);
+    meet6.append(after6);
+    
+    <!--- --->
+
+    let meet7 = document.getElementById("meetings-title7");
+    let before7 = document.createElement("span");
+    before7.classList.add("navigation");
+    before7.innerHTML = '&larr; Advanced Vir';
+    let after7 = document.createElement("span");
+    after7.classList.add("navigation");
+    after7.innerHTML = 'tual Reality &rarr;';
+    meet7.innerHTML = '';
+    meet7.prepend(before7);
+    meet7.append(after7);    
+    
+    <!--- --->
+
+    let meet8 = document.getElementById("meetings-title8");
+    let before8 = document.createElement("span");
+    before8.classList.add("navigation");
+    before8.innerHTML = '&larr; Computer ';
+    let after8 = document.createElement("span");
+    after8.classList.add("navigation");
+    after8.innerHTML = 'Graphics 1 &rarr;';
+    meet8.innerHTML = '';
+    meet8.prepend(before8);
+    meet8.append(after8); 
 
     
 
