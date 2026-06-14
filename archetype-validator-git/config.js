4d83e36e1235a7185a3286df88eafc0b13d77c3d
richtrad <35641706+richtrad@users.noreply.github.com>
2026-06-14 18:59:08 +0200

    Update config.js

--- archetype-validator-git/config.js
+++ archetype-validator-git/config.js
@@ -1,7 +1,7 @@
 window.ARCHETYPE_CONFIG = {
   // Sem vlož URL z Google Apps Script deploymentu končící /exec.
   // Když zůstane prázdné, web bude ukládat jen lokálně do prohlížeče.
-  GOOGLE_SHEETS_WEB_APP_URL: '',
+  GOOGLE_SHEETS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzDCTDaYozWqU6Gn1GqUJSyY3tEpehhtwCn9KS7MzPnlDDD8Jx1Rak5Di8ZfP5x9KPNMQ/exec',
 
   // true = současně ukládat záložní kopii do localStorage
   KEEP_LOCAL_BACKUP: true,
