833dfe855d97a4697e7bedcdfb9cb4e211d2b3d4
richtrad <35641706+richtrad@users.noreply.github.com>
2026-06-27 00:36:47 +0200

    Add VIP validator config

---
+++ archetype-validator-vip/config.js
@@ -0,0 +1,7 @@
+window.ARCHETYPE_CONFIG = {
+  GOOGLE_SHEETS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzDCTDaYozWqU6Gn1GqUJSyY3tEpehhtwCn9KS7MzPnlDDD8Jx1Rak5Di8ZfP5x9KPNMQ/exec',
+  KEEP_LOCAL_BACKUP: true,
+  MAX_SELECTIONS: 3,
+  VALIDATOR_VARIANT: 'vip',
+  RESPONSE_SHEET_NAME: 'responses_vip'
+};
